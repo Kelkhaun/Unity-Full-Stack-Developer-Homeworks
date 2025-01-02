5dc9f953d9756dc491d1b97becffacb4c082d38b
Ilya Shilov <80789057+Kelkhaun@users.noreply.github.com>
2025-01-02 12:49:14 +0300

    Update PlanetPopupPresenter.cs

--- Assets/Game/Scripts/Gameplay/PlanetPopup/PlanetPopupPresenter.cs
+++ Assets/Game/Scripts/Gameplay/PlanetPopup/PlanetPopupPresenter.cs
@@ -5,27 +5,29 @@ namespace Game.Scripts.Gameplay.PlanetPopup
 {
     public class PlanetPopupPresenter : IPlanetPopupPresenter
     {
-        private IPlanet _planet;
-        private IMoneyAdapter _moneyAdapter;
-
-        public string PlanetName => _planet.Name;
-        public string PlanetPopulation => "Population: " + _planet.Population;
-        public string PlanetLevel =>"Level: " +  _planet.Level + "/" + _planet.MaxLevel;
-
-        public string PlanetIncome => "Income: " + _planet.MinuteIncome + " / sec";
-
-        public string PlanetPrice => _planet.Price != Planet.UNDEFINED_PRICE
-            ? _planet.Price.ToString()
-            : "Max Level";
-        public bool IsButtonActive => _moneyAdapter.IsEnough(_planet.Price) && _planet.MaxLevel > _planet.Level;
-        
         public event Action OnPlanetUpgrade;
+
         public event Action<int> OnPopulationChanged
         {
             add => _planet.OnPopulationChanged += value;
             remove => _planet.OnPopulationChanged -= value;
         }
-        
+
+        private IPlanet _planet;
+
+        private IMoneyAdapter _moneyAdapter;
+
+        public string PlanetName => _planet.Name;
+        public string PlanetPopulation => "Population: " + _planet.Population;
+        public string PlanetLevel => "Level: " + _planet.Level + "/" + _planet.MaxLevel;
+        public string PlanetIncome => "Income: " + _planet.MinuteIncome + " / sec";
+        public string PlanetPrice => _planet.Price != Planet.UNDEFINED_PRICE
+            ? _planet.Price.ToString()
+            : "Max Level";
+
+        public bool IsButtonActive =>
+            _moneyAdapter.IsEnough(_planet.Price) && _planet.MaxLevel > _planet.Level;
+
         public PlanetPopupPresenter(IPlanet planet, IMoneyAdapter moneyAdapter)
         {
             _planet = planet;
